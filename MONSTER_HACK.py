fc77cb6ab0f9e2fe6f2c85c4697126159c3bad3a
MONSTER-hp <69053807+MONSTER-hp@users.noreply.github.com>
2020-07-31 23:51:31 +0430

    Create MONSTER_HACK.py
    Monster_hack

---
+++ MONSTER_HACK.py
@@ -0,0 +1,269 @@
+import os
+os.system('clear')
+os.system('clear')
+os.system('clear')
+os.system('clear')
+print"""\033[92m
+ __  __  ___  _   _ ____ _____ _____ ____      _   _    _    ____ _  __ 
+|  \/  |/ _ \| \ | / ___|_   _| ____|  _ \    | | | |  / \  / ___| |/ / 
+| |\/| | | | |  \| \___ \ | | |  _| | |_) |   | |_| | / _ \| |   | ' /  
+| |  | | |_| | |\  |___) || | | |___|  _ <    |  _  |/ ___ \ |___| . \  
+|_|  |_|\___/|_| \_|____/ |_| |_____|_| \_\___|_| |_/_/   \_\____|_|\_\ 
+                                         |_____|                        
+"""
+print"""\033[96m
+        >>> Telegram Chanell : https://t.me/MONSTER_SECURIT <<<
+"""
+print"""\033[93m
+          >>> Web Sayte : www.Monster-Security.blogfa.com <<<
+"""
+print"""\033[92m
+                         </> MONSTER_hp </>
+"""
+print"""\033[91m
+
+========================================================================
+                                  Web
+========================================================================
+"""
+print"""\033[94m
+ [1]: </> Hammer </>
+ [2]: </> Hacked16-Ddos </>
+ [3]: </> OwScan </>
+ [4]: </> RED_HAWK </>
+ [5]: </> RapidScan </>
+ [6]: </> Admin_Penal </>
+"""
+print"""\033[93m
+========================================================================
+                                 Bombers
+========================================================================
+"""
+print"""\033[96m
+ [7]: </> SpamSms </>
+ [8]: </> Email-Spammer </>
+ [9]: </> SpazSMS </>
+ [10]: </> TBomb </>
+"""
+print"""\033[95m
+========================================================================
+                                Hacking
+========================================================================
+"""
+print"""\033[91m
+ [11]: </> Cam_Hackers </>
+ [12]: </> IP-tracer </>
+ [13]: </> IPGeoLocation </>
+ [14]: </> Cam </>
+ [15]: </> HackVoice </>
+ [16]: </> Fakepage </>
+ [17]: </> Zphisher </>
+"""
+print"""\033[90m
+========================================================================
+                                Cracker
+========================================================================
+"""
+print"""\033[92m
+ [18]: </> Black_Hydra </>
+ [19]: </> Gmail_Attacker </>
+ [20]: </> Instahack </>
+"""
+
+
+x= input('Enter Number For Install :')
+
+if x== 1:
+	os.system("pkg install git")
+	os.system("pkg install python")
+	os.system("pkg install python2")
+	os.system("git clone https://github.com/cyweb/hammer")
+	os.system("cd hammer")
+	os.system("chmod +x *")
+	os.system("python hammer.py")
+
+if x== 2:
+	os.system("pkg install git")
+	os.system("pkg install python")
+	os.system("pkg install python2")
+	os.system("git clone https://github.com/Mahdiihack/Hacked16-Ddos")
+	os.system("cd Hacked16-Ddos")
+	os.system("chmod +x *")
+	os.system("python2 Hacked16-Ddos.py")
+	
+if x== 3:
+	os.system("pkg install git")
+	os.system("pkg install php")
+	os.system("git clone https://github.com/Gameye98/OWScan")
+	os.system("cd OWScan")
+	os.system("chmod +x *")
+	os.system("php owscan.php")
+
+if x== 4:
+	os.system("pkg install git")
+	os.system("pkg install php")
+	os.system("git clone https://github.com/Tuhinshubhra/RED_HAWK")
+	os.system("cd RED_HAWK")
+	os.system("chmod +x *")
+	os.system("php rhawk.php")
+	
+if x== 5:
+	os.system("pkg install git")
+	os.system("pkg install wget")
+	os.system("pkg install python")
+	os.system("pkg install python2")
+	os.system("git clone https://github.com/skavngr/rapidscan")
+	os.system("cd rapidscan")
+	os.system("chmod +x *")
+	os.system("wget -O rapidscan.py")
+	os.system("python2 rapidscan.py")
+	
+if x== 6:
+	os.system("pkg install git")
+	os.system("pkg install python")
+	os.system("pkg install python2")
+	os.system("git clone https://github.com/Techzindia/admin_penal")
+	os.system("cd admin_panel")
+	os.system("chmod +x *")
+	os.system("python2 admin_panel_finder.py")
+	
+
+if x== 7:
+	os.system("pkg install git")
+	os.system("pkg install python")
+	os.system("pkg install python2")
+	os.system("git clone https://github.com/KANG-NEWBIE/SpamSms")
+	os.system("cd spamsms")
+	os.system("chmod +x *")
+	os.system("python main.py")
+	
+if x== 8:
+	os.system("pkg install git")
+	os.system("pkg install sh")
+	os.system("pkg install fish")
+	os.system("pkg install python")
+	os.system("pkg install python2")
+	os.system("https://github.com/Juniorn1003/Email-Spammer")
+	os.system("cd Email-Spammer")
+	os.system("chmod +x *")
+	os.system("bash install.sh")
+	os.system("python2 Email-Spam.py")
+	
+if x== 9:
+	os.system("pkg install git")
+	os.system("pkg install python")
+	os.system("pkg install python2")
+	os.system("git clone https://github.com/Gameye98/SpazSMS")
+	os.system("cd SpazSMS")
+	os.system("chmod +x *")
+	os.system("python2 phd.py")
+	
+if x== 10:
+	os.system("pkg install git")
+	os.system("pkg install sh")
+	os.system("pkg install fish")
+	os.system("pkg install pip")
+	os.system("git clone https://github.com/TheSpeedX/TBomb")
+	os.system("cd TBomb")
+	os.system("chmod +x *")
+	os.system("pip install -r requirements.txt")
+	os.system("bash TBomb.sh")
+	os.system("TBomb")
+	
+if x== 11:
+	os.system("pkg install git")
+	os.system("pkg install python")
+	os.system("pkg install python2")
+	os.system("git clone https://github.com/AngelSecurityTeam/Cam-Hackers")
+	os.system("cd Cam-Hackers")
+	os.system("chmod +x *")
+	os.system("python cam-hackers.py")
+	
+if x== 12:
+	os.system("pkg install git")
+	os.system("pkg install fish")
+	os.system("pkg install sh")
+	os.system("git clone https://github.com/rajkumardusad/IP-Tracer")
+	os.system("cd IP-Tracer")
+	os.system("chmod +x *")
+	os.system("bash install")
+	
+if x== 13:
+	os.system("pkg install git")
+	os.system("pkg install python")
+	os.system("pkg install python2")
+	os.system("git clone git clone https://github.com/maldevel/IPGeoLocation")
+	os.system("cd IPGeoLocation")
+	os.system("chmod +x *")
+	os.system("pip install -r requirements.txt")
+	os.system("python IPGeoLocation.py")
+	
+if x== 14:
+	os.system("pkg install git")
+	os.system("pkg install php")
+	os.system("pkg install wget")
+	os.system("pkg install sh")
+	os.system("pkg install fish")
+	os.system("pkg install openssl")
+	os.system("pkg install curl")
+	os.system("git clone https://github.com/HACKGM/cam")
+	os.system("cd cam")
+	os.system("chmod +x *")
+	os.system("bash cam.sh")
+	
+if x== 15:
+	os.system("pkg install git")
+	os.system("pkg install php")
+	os.system("pkg install wget")
+	os.system("pkg install openssl")
+	os.system("pkg install sh")
+	os.system("pkg install fish")
+	os.system("git clone https://github.com/HACKGM/hackvoice")
+	os.system("cd hackvoice")
+	os.system("chmod +x *")
+	os.system("bash voice.sh")
+	
+if x== 16:
+	os.system("pkg install git")
+	os.system("git clone https://github.com/HACKGM/fakepage")
+	os.system("cd fakepage")
+	os.system("chmod +x *")
+	os.system("bash fakepage.sh")
+	
+if x== 17:
+	os.system("pkg install git")
+	os.system("pkg install sh")
+	os.system("pkg install fish")
+	os.system("pkg install openssh")
+	os.system("git clone https://github.com/htr-tech/zphisher")
+	os.system("cd zphisher")
+	os.system("chmod +x *")
+	os.system("bash zphisher.sh")
+	
+if x== 18:
+	os.system("pkg install git")
+	os.system("pkg install python")
+	os.system("pkg install python2")
+	os.system("git clone https://github.com/Gameye98/Black-Hydra")
+	os.system("cd Black-Hydra")
+	os.system("chmod +x *")
+	os.system("python2 Black-Hydra.py")
+	
+if x== 19:
+	os.system("pkg install git")
+	os.system("pkg install python")
+	os.system("pkg install python2")
+	os.system("git clone https://github.com/AyoubSirai/gmail_attacker")
+	os.system("cd gmail_attacker")
+	os.system("chmod +x *")
+	os.system("python gmail_attacker.py")
+	
+if x== 20:
+	os.system("pkg install git")
+	os.system("pkg install python")
+	os.system("pkg install python2")
+	os.system("pkg install pip")
+	os.system("git clone https://github.com/fuck3erboy/instahack")
+	os.system("cd instahack")
+	os.system("chmod +x *")
+	os.system("python hackinsta.py")
